874b8ae1bf337bb993d81eb5079c18b16534f326
Kyle Sendelbach <sendelbach.18@osu.edu>
2024-11-19 18:53:43 -0500

    pages 1-4

--- LAB 3 4630/web-project-essay/frontend/src/Products/Products.tsx
+++ LAB 3 4630/web-project-essay/frontend/src/Products/Products.tsx
@@ -1,9 +1,39 @@
-import data from "./data";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+type Product = {
+  id: number;
+  name: string;
+  description: string;
+  brand: string;
+  price: string;
+  rating: number;
+  numberOfReviews: number;
+  imageUrl: string;
+}
+
+const emptyProducts: Product[] = [];
+
 const Products = () => {
+  const [products, setProducts]: [Product[], (products: Product[]) => void]
+  = useState(emptyProducts);
+
+  useEffect(() => {
+    axios.get<Product[]>("http://localhost:5196/catalog",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => setProducts(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+
   return (
     <div className="content">
       <ul className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <li className="product">
             <div className="product">
               <img
